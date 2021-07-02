6ab0d4e83abd6fee3bee824b06dbd12604a82d58
cassiano yasumitsu <cassiano@usuarioincomum.com>
2021-07-02 10:21:30 +0900

    rubocop

--- template.rb
+++ template.rb
@@ -22,70 +22,70 @@ end
 
 gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 
-# Assets
-########################################
-run 'rm -rf app/assets/stylesheets'
-run 'rm -rf vendor'
-run 'curl -L https://github.com/lewagon/rails-stylesheets/archive/master.zip > stylesheets.zip'
-run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
+# # Assets
+# ########################################
+# run 'rm -rf app/assets/stylesheets'
+# run 'rm -rf vendor'
+# run 'curl -L https://github.com/lewagon/rails-stylesheets/archive/master.zip > stylesheets.zip'
+# run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/rails-stylesheets-master app/assets/stylesheets'
 
-# Dev environment
-########################################
-gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+# # Dev environment
+# ########################################
+# gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
 
-# Layout
-########################################
-if Rails.version < "6"
-  scripts = <<~HTML
-    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>
-        <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
-  HTML
-  gsub_file('app/views/layouts/application.html.erb', "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>", scripts)
-end
+# # Layout
+# ########################################
+# if Rails.version < "6"
+#   scripts = <<~HTML
+#     <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>
+#         <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
+#   HTML
+#   gsub_file('app/views/layouts/application.html.erb', "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>", scripts)
+# end
 
-gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
+# gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
 
-style = <<~HTML
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
-HTML
-gsub_file('app/views/layouts/application.html.erb', "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", style)
+# style = <<~HTML
+#   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+#       <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+# HTML
+# gsub_file('app/views/layouts/application.html.erb', "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", style)
 
-# Flashes
-########################################
-file 'app/views/shared/_flashes.html.erb', <<~HTML
-  <% if notice %>
-    <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
-      <%= notice %>
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  <% end %>
-  <% if alert %>
-    <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
-      <%= alert %>
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  <% end %>
-HTML
+# # Flashes
+# ########################################
+# file 'app/views/shared/_flashes.html.erb', <<~HTML
+#   <% if notice %>
+#     <div class="alert alert-info alert-dismissible fade show m-1" role="alert">
+#       <%= notice %>
+#       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+#         <span aria-hidden="true">&times;</span>
+#       </button>
+#     </div>
+#   <% end %>
+#   <% if alert %>
+#     <div class="alert alert-warning alert-dismissible fade show m-1" role="alert">
+#       <%= alert %>
+#       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+#         <span aria-hidden="true">&times;</span>
+#       </button>
+#     </div>
+#   <% end %>
+# HTML
 
-run 'curl -L https://github.com/lewagon/awesome-navbars/raw/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb'
+# run 'curl -L https://github.com/lewagon/awesome-navbars/raw/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb'
 
-inject_into_file 'app/views/layouts/application.html.erb', after: '<body>' do
-  <<-HTML
+# inject_into_file 'app/views/layouts/application.html.erb', after: '<body>' do
+#   <<-HTML
 
-    <%= render 'shared/navbar' %>
-    <%= render 'shared/flashes' %>
-  HTML
-end
+#     <%= render 'shared/navbar' %>
+#     <%= render 'shared/flashes' %>
+#   HTML
+# end
 
 # README
 ########################################
 markdown_file_content = <<-MARKDOWN
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+Rails app generated by Cassiano
 MARKDOWN
 file 'README.md', markdown_file_content, force: true
 
@@ -216,3 +216,11 @@ after_bundle do
   # Fix puma config
   gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
 end
+
+
+
+# rails new \
+#   --database postgresql \
+#   --webpack \
+#   -m https://raw.githubusercontent.com/cassianotadaoyasumitsu/cassiano/master/template.rb \
+#   CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
