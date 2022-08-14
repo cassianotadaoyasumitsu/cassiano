162883a03b2a98972f07f70a511c3a7aa062ee44
Cassiano Tadao Yasumitsu <cassiano@usuarioincomum.com>
2022-08-15 08:07:09 +0900

    Update template2.rb

--- template2.rb
+++ template2.rb
@@ -187,7 +187,7 @@ after_bundle do
 
   # Rubocop
   ########################################
-  run "curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml"
+  run "curl -L https://raw.githubusercontent.com/cassianotadaoyasumitsu/cassiano/master/.rubocop.yml > .rubocop.yml"
 
   # Git
   ########################################
