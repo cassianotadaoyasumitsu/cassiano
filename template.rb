1843fb9c6744cc0b7b48f41e5c299d2dead4ae61
Cassiano Tadao Yasumitsu <cassiano@usuarioincomum.com>
2024-02-20 11:34:17 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -72,7 +72,7 @@ file 'app/views/shared/_flashes.html.erb', <<~HTML
   <% end %>
 HTML
 
-run 'curl -L https://github.com/lewagon/awesome-navbars/raw/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb'
+run 'curl -L https://raw.githubusercontent.com/cassianotadaoyasumitsu/cassiano/master/_navbar.html.erb'
 
 inject_into_file 'app/views/layouts/application.html.erb', after: '<body>' do
   <<-HTML
@@ -206,7 +206,7 @@ after_bundle do
 
   # Rubocop
   ########################################
-  run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
+  run 'curl -L https://raw.githubusercontent.com/cassianotadaoyasumitsu/cassiano/master/.rubocop.yml > .rubocop.yml'
 
   # Git
   ########################################
